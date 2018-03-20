6e2a006ac7585951e7f98793ad061bb5a889e2ea
Adam Jurík <ja004@seznam.cz>
2018-03-20 13:27:11 +0100

    Initial commit

--- ForestReco/Program.cs
+++ ForestReco/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,21 @@ namespace ForestReco
 	{
 		static void Main(string[] args)
 		{
+			// Example #1
+			// Read the file as one string.
+			//string text = File.ReadAllText(@"D:\Adam\projects\SDIPR\ForestReco\ForestReco\src\ANE_1000AGL.las");
+			
+			string[] lines = File.ReadAllLines(@"D:\Adam\projects\SDIPR\ForestReco\ForestReco\src\ANE_1000AGL.las");
+			
+			//foreach (string line in lines)
+			for(int i = 0; i < 5; i++)
+			{
+				// Use a tab to indent each line of the file.
+				Console.WriteLine(lines[i]);
+			}
+			
+			Console.WriteLine("Press any key to exit.");
+			System.Console.ReadKey();
 		}
 	}
 }
