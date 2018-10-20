02b9a5b1cd6c885d0c2b0478f2204c03ced0d1b2
Adam Jurík <ja004@seznam.cz>
2018-10-20 11:58:54 +0200

    Parameter setter

---
+++ ForestReco/Controllers/CParameterSetter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Security;
+using System.Windows.Forms;
+
+namespace ForestReco
+{
+	public static class CParameterSetter
+	{
+		public static string forrestFilePath;
+
+
+
+		public static void Init()
+		{
+			bool containsForrestPathKey = Properties.Settings.Default.SettingsKey.Contains("forrestFilePath");
+			if (containsForrestPathKey)
+			{
+				object forrestFilePathSettings = Properties.Settings.Default["forrestFilePath"];
+				forrestFilePath = (string)forrestFilePathSettings;
+			}
+		}
+
+		private static string SetForrestFilePath(string pPath)
+		{
+			forrestFilePath = pPath;
+			Properties.Settings.Default["forrestFilePath"] = pPath;
+			Properties.Settings.Default.Save();
+			return pPath;
+		}
+
+		public static string SelectForrestFile()
+		{
+			OpenFileDialog ofd = new OpenFileDialog();
+			//ofd.Multiselect = true;
+			//ofd.InitialDirectory = Environment.CurrentDirectory;
+			ofd.RestoreDirectory = true;
+			ofd.Title = "Open forrest file";
+			ofd.ShowHelp = true;
+			DialogResult dr = ofd.ShowDialog();
+			if (dr == DialogResult.OK)
+			{
+				return SetForrestFilePath(ofd.FileName);
+
+				/*foreach (String file in ofd.FileNames)
+				{
+					try
+					{
+						if ())
+						{
+							
+						}
+						else
+						{
+							MessageBox.Show("selected file: " + file.ToString() + " is not an image!");
+						}
+
+					}
+					catch (SecurityException ex)
+					{
+						// The user lacks appropriate permissions to read files, discover paths, etc.
+						MessageBox.Show("Security error. \n\n" + "Error message: " + ex.Message + "\n\n" + ex.StackTrace
+						);
+					}
+					catch (Exception ex)
+					{
+						// Could not load the image - probably related to Windows file system permissions.
+						MessageBox.Show("Cannot load the image: " + file.Substring(file.LastIndexOf('\\'))
+							 + ". You may not have permission to read the file, or " +
+							 "it may be corrupt.\n\nReported error: " + ex.Message);
+					}
+				}*/
+			}
+			return "";
+		}
+	}
+}
--- ForestReco/Program.cs
+++ ForestReco/Program.cs
@@ -9,35 +9,85 @@ using System.Windows.Forms;
 
 namespace ForestReco
 {
-	public partial class Program : Form
+	public class Program : Form
 	{
+		private TextBox textForrestFilePath;
+		private Button btnSellectForrest;
+
 		public Program()
 		{
 			InitializeComponent();
+			InitializeValues();
 		}
 
-		static void Main()
+
+		[STAThread]
+		static void Main(string[] args)
 		{
 			Application.Run(new Program());
+
+			return;
 			CProgramStarter.Start();
 		}
 
 
+
+		private void InitializeValues()
+		{
+			CParameterSetter.Init();
+			textForrestFilePath.Text = CParameterSetter.forrestFilePath;
+		}
+
 		private void InitializeComponent()
 		{
+			this.btnSellectForrest = new System.Windows.Forms.Button();
+			this.textForrestFilePath = new System.Windows.Forms.TextBox();
 			this.SuspendLayout();
+			// 
+			// btnSellectForrest
+			// 
+			this.btnSellectForrest.Location = new System.Drawing.Point(0, 0);
+			this.btnSellectForrest.Name = "btnSellectForrest";
+			this.btnSellectForrest.Size = new System.Drawing.Size(116, 31);
+			this.btnSellectForrest.TabIndex = 0;
+			this.btnSellectForrest.Text = "select forrest file";
+			this.btnSellectForrest.UseVisualStyleBackColor = true;
+			this.btnSellectForrest.Click += new System.EventHandler(this.btnSellectForrest_Click);
+			// 
+			// textForrestFilePath
+			// 
+			this.textForrestFilePath.Location = new System.Drawing.Point(122, 6);
+			this.textForrestFilePath.Name = "textForrestFilePath";
+			this.textForrestFilePath.Size = new System.Drawing.Size(341, 20);
+			this.textForrestFilePath.TabIndex = 1;
+			this.textForrestFilePath.TextChanged += new System.EventHandler(this.textForrestFilePath_TextChanged);
 			// 
 			// Program
 			// 
 			this.ClientSize = new System.Drawing.Size(475, 373);
+			this.Controls.Add(this.textForrestFilePath);
+			this.Controls.Add(this.btnSellectForrest);
 			this.Name = "Program";
 			this.Text = "ForrestReco";
 			this.Load += new System.EventHandler(this.Program_Load);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		private void Program_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnSellectForrest_Click(object sender, EventArgs e)
+		{
+			string path = CParameterSetter.SelectForrestFile();
+			textForrestFilePath.Clear();
+			textForrestFilePath.Text = path;
+		}
+
+		private void textForrestFilePath_TextChanged(object sender, EventArgs e)
 		{
 
 		}
